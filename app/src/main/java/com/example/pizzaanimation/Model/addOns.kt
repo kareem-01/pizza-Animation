5ba1a0a7b6d39db35cb1f05f63fd0659694efab5
kareem <kareemesam52@yahoo.com>
2023-07-19 06:47:37 +0300

    update addOn UiState

--- app/src/main/java/com/example/pizzaanimation/Model/addOns.kt
+++ app/src/main/java/com/example/pizzaanimation/Model/addOns.kt
@@ -1,6 +1,10 @@
 package com.example.pizzaanimation.Model
 
+import com.example.pizzaanimation.R
+
 data class AddOns(
     val name : String,
+    val imageId:Int,
     var isSelected:Boolean = false,
+    val addOnsGroupId:Int = R.drawable.group_basil
 )
