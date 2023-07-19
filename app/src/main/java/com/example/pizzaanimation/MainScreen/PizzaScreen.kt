cd9d60b939bb780a180687cba7e56fc6d0b8099a
kareem <kareemesam52@yahoo.com>
2023-07-19 06:48:50 +0300

    update screen

--- app/src/main/java/com/example/pizzaanimation/MainScreen/PizzaScreen.kt
+++ app/src/main/java/com/example/pizzaanimation/MainScreen/PizzaScreen.kt
@@ -1,21 +1,30 @@
 package com.example.pizzaanimation.MainScreen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.SpringSpec
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
@@ -32,6 +41,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,6 +50,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.pizzaanimation.MainScreen.ViewModel.PizzaViewModel
 import com.example.pizzaanimation.MainScreen.composables.HorizontalSpacer
+import com.example.pizzaanimation.MainScreen.composables.IngredientButton
 import com.example.pizzaanimation.MainScreen.composables.SizeButton
 import com.example.pizzaanimation.Model.PizzaSelectionUiState
 import com.example.pizzaanimation.Model.PizzaSize
@@ -77,27 +88,33 @@ fun PizzaSelectionScreen(
                 )
             }
         },
-        pizzaSize
+        pizzaSize = pizzaSize,
+        onIngredientClick = { addOnInex ->
+            viewModel.ingredientClick(pizza = pagerState.currentPage, addOn = addOnInex)
+        }
     )
 
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun PizzaSelectionContent(
     pizzaUiState: PizzaSelectionUiState,
     pagerState: PagerState,
     sizeButtonClick: (size: PizzaSize) -> Unit,
     pizzaSize: Int,
+    onIngredientClick: (id: Int) -> Unit,
 ) {
     val currentSize = pizzaUiState.pizzas[pagerState.currentPage].pizzaSize
-    val currentSizeDp = when (currentSize) {
-        PizzaSize.Small -> 220.dp
-        PizzaSize.Medium -> 240.dp
-        PizzaSize.Large -> 260.dp
+    val currentSizeScale = when (currentSize) {
+        PizzaSize.Small -> .8f
+        PizzaSize.Medium -> .9f
+        PizzaSize.Large -> 1f
     }
-    val size by animateDpAsState(targetValue = currentSizeDp,
-  tween(400, easing = LinearEasing)  )
+    val size by animateFloatAsState(
+        targetValue = currentSizeScale,
+        tween(350, easing = LinearEasing)
+    )
     LaunchedEffect(key1 = pizzaUiState.pizzas[pagerState.currentPage].pizzaSize) {
 
     }
@@ -126,7 +143,6 @@ fun PizzaSelectionContent(
                 contentDescription = null,
                 modifier = Modifier
                     .padding(42.dp)
-                    .size(350.dp)
             )
             HorizontalPager(pageCount = pizzaUiState.pizzas.size, state = pagerState) { page ->
                 Box(
@@ -137,12 +153,12 @@ fun PizzaSelectionContent(
                                 stiffness = Spring.StiffnessLow
                             )
                         )
-                        .size(
-                          size
-                        )
+                        .scale(size),
+                    contentAlignment = Alignment.Center
                 ) {
                     Image(
                         modifier = Modifier
+                            .align(Alignment.Center)
                             .animateContentSize(
                                 animationSpec = SpringSpec(
                                     stiffness = Spring.StiffnessLow
@@ -151,11 +167,28 @@ fun PizzaSelectionContent(
                         painter = painterResource(id = pizzaUiState.pizzas[page].breadType),
                         contentDescription = null,
                     )
+                    pizzaUiState.pizzas[pagerState.currentPage].addOns.forEach { addOn ->
+                        androidx.compose.animation.AnimatedVisibility(
+                            visible = addOn.isSelected && !pagerState.isScrollInProgress,
+                            enter = scaleIn(initialScale = 50f) + fadeIn(),
+                            exit = ExitTransition.None
+                        ) {
+                            Image(
+                                painter = painterResource(id = addOn.addOnsGroupId),
+                                contentDescription = null
+                            )
+                        }
+                    }
                 }
 
             }
         }
 
+        Text(
+            text = '$' + pizzaUiState.pizzas[pagerState.currentPage].price.toString(),
+            modifier = Modifier.align(Alignment.CenterHorizontally).padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             SizeButton(
                 size = PizzaSize.Small,
@@ -176,7 +209,25 @@ fun PizzaSelectionContent(
             )
         }
 
-        Text(text = "Customize Your Pizza")
+        Text(
+            text = "Customize Your Pizza",
+            modifier = Modifier.padding(top = 16.dp),
+            fontWeight = FontWeight.SemiBold
+        )
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = 24.dp),
+            horizontalArrangement = Arrangement.spacedBy(32.dp)
+        ) {
+            val addOns = pizzaUiState.pizzas[pagerState.currentPage].addOns
+            items(count = addOns.size) { ingredientId ->
+                IngredientButton(
+                    imageId = addOns[ingredientId].imageId,
+                    addOnId = ingredientId,
+                    onIngredientClick = onIngredientClick,
+                    isSelected = addOns[ingredientId].isSelected
+                )
+            }
+        }
 
     }
 }
